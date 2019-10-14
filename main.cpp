0fa530355097fa5382977a85b40440cbbad65d54
Paula Ribeiro <paula.ribeiro@dtidigital.com.br>
2019-10-14 17:08:50 -0300

    Entrada de dados

---
+++ main.cpp
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <iostream>
+
+FILE* abrirArquivo(int argc, char *argv[]);
+int recuperarNumero(FILE* arquivo);
+
+int main(int argc, char *argv[]) {
+
+    FILE* arquivo = abrirArquivo(argc, argv);
+    if (arquivo == NULL)
+        return 1;
+
+    int gastoMax, qtdIlhas;
+
+    gastoMax = recuperarNumero(arquivo);
+    qtdIlhas = recuperarNumero(arquivo);
+
+    int ilhas[qtdIlhas][2];
+
+    for (int i=0; i<qtdIlhas;i++) {
+        ilhas[i][0] = recuperarNumero(arquivo);
+        ilhas[i][1] = recuperarNumero(arquivo);
+    }
+
+	fclose(arquivo);
+
+    return 0;
+}
+
+FILE* abrirArquivo(int argc, char *argv[]) {
+    if (argc < 2) {
+        std::cout << "Parâmetro inválido.";
+        return NULL;
+    }
+
+    char* nomeArquivo = argv[1];
+
+    FILE* arquivo = fopen(nomeArquivo, "r");
+
+    return arquivo;
+}
+
+int recuperarNumero(FILE* arquivo) {
+    int numero = 0, auxiliar;
+
+    //enquanto char lido nao for espaco branco ou nova linha
+    while ((auxiliar = fgetc(arquivo)) != ' ' && auxiliar != '\n') {
+        numero*=10;
+        //transforma char em valor numerico
+        numero+=auxiliar-48;
+    }
+
+    return numero;
+}
